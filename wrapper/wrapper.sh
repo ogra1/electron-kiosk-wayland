6e7767043543a93cf552cd77d51a6a3aa2b0835f
jmbrock <joseph.brock@canonical.com>
2023-01-11 11:18:37 -0500

    Enable Wayland OSK support by default

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -10,4 +10,5 @@ exec $SNAP/electron-helloworld/electron-quick-start \
 	--enable-features=UseOzonePlatform \
 	--ozone-platform=wayland \
 	--disable-dev-shm-usage \
+	--enable-wayland-ime \
 	--no-sandbox $EXTRAOPTS
